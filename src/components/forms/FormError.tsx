2a4c0ae370a8451651c9fdaf568756ea36faba0c
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-11-23 18:18:38 -0700

    Fixed DisplayFormError

--- src/components/forms/FormError.tsx
+++ src/components/forms/FormError.tsx
@@ -4,31 +4,48 @@ import z from "zod";
 export type FormError =
   | z.ZodError
   | { message: string }
-  | { errors: { path: (string | number)[]; message: string }[] }
+  | { path: (string | number)[]; message: string }[]
   | string
   | null;
 
 export default function DisplayFormError({
   error,
   className,
+  itemClassName,
 }: {
   error: FormError;
   className?: string;
+  itemClassName?: string;
 }) {
   if (!error) return <span></span>;
 
   if (typeof error === "string")
     return <span className={className}>{error}</span>;
 
-  if ("message" in error)
-    return <span className={className}>{error.message}</span>;
+  if ("message" in error) {
+    try {
+      const errors: { path: string[]; message: string }[] = JSON.parse(
+        error.message
+      );
+      return (
+        <ul className={className}>
+          {errors.map((e, idx) => (
+            <li key={idx} className={itemClassName}>
+              {e.path.join(", ")} - {e.message}
+            </li>
+          ))}
+        </ul>
+      );
+    } catch {
+      return <span className={className}>{error.message}</span>;
+    }
+  }
 
-  if (error instanceof z.ZodError || "errors" in error)
+  if (error instanceof z.ZodError || Array.isArray(error))
     return (
       <ul className={className}>
-        <li>Is Zod Error</li>
-        {error.errors.map((e, idx) => (
-          <li key={idx}>
+        {error.map((e, idx) => (
+          <li key={idx} className={itemClassName}>
             {e.path.join(", ")} - {e.message}
           </li>
         ))}
